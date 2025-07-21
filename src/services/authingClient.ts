46e1f2908719b4cbfdcc3e9c2206768b9c0e63aa
xiongtingping <98273729+xiongtingping@users.noreply.github.com>
2025-07-22 00:44:16 +0800

    fix(authing): use import * as AuthingWeb from '@authing/web' to avoid require in browser bundle

--- src/services/authingClient.ts
+++ src/services/authingClient.ts
@@ -4,9 +4,7 @@
  * 参考: https://docs.authing.cn/v2/quickstarts/spa/react.html
  */
 
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const AuthingWeb = require('@authing/web');
+import * as AuthingWeb from '@authing/web';
 import { getAuthingConfig } from '@/config/authing';
 
 /**
